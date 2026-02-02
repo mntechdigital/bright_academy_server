016a3cc945e472fb123ceb6b6ef48415dd1528ee
reshad21 <miakala7@gmail.com>
2026-02-02 11:35:53 +0600

    feat: Update role features and paths to include leading slashes for consistency

--- src/app/constant/seedRoleData.ts
+++ src/app/constant/seedRoleData.ts
@@ -4,20 +4,24 @@ import { TAdminRole } from "../types/adminRole.types";
 export const seedRoleAdminData: TAdminRole = {
   name: 'Super Admin',
   roleFeature: [
-    { name: 'Auth', path: 'auth', index: 1 },
-    { name: 'Roles & Permissions', path: 'roles_permissions', index: 2 },
-    { name: 'Student Classes', path: 'studentClasses', index: 3 },
-    { name: 'Student Sections', path: 'studentSections', index: 4 },
-    { name: 'Subjects', path: 'subjects', index: 5 },
-    { name: 'Students', path: 'students', index: 6 },
+    { name: 'Auth', path: '/auth', index: 1 },
+    { name: 'Roles & Permissions', path: '/roles-permissions', index: 2 },
+    { name: 'Classes', path: '/classes', index: 3 },
+    { name: 'Sections', path: '/sections', index: 4 },
+    { name: 'Subjects', path: '/subjects', index: 5 },
+    { name: 'Students', path: '/students', index: 6 },
+    { name: 'Teachers', path: '/teachers', index: 7 },
+    { name: 'Result', path: '/result', index: 8 },
   ],
 };
 
 export const featureNames = {
-  auth: 'auth',
-  rolesAndPermissions: 'roles_permissions',
-  studentClasses: 'studentClasses',
-  studentSections: 'studentSections',
-  subjects: 'subjects',
-  students: 'students',
+  auth: '/auth',
+  rolesAndPermissions: '/roles-permissions',
+  classes: '/classes',
+  sections: '/sections',
+  subjects: '/subjects',
+  students: '/students',
+  teachers: '/teachers',
+  result: '/result',
 };
